0581585d145b011c46b15fd9559dd8c53d260e23
BrenoPocas <brenomouraopocas@gmail.com>
2024-09-01 13:30:22 -0300

    funçoes js adicionadas

--- Prof Diego/Dia 08-29/Atividade 1/Breno Mourao/scripts/script.js
+++ Prof Diego/Dia 08-29/Atividade 1/Breno Mourao/scripts/script.js
@@ -1,12 +1,23 @@
 let titulos = document.querySelectorAll(".titulo")
 let corpos = document.querySelectorAll(".corpo")
+let backgroundList = [
+    "./img/default-wallpaper.jpg", //DEFAULT
+    "./img/arte-e-design.png", 
+    "./img/educacao-e-cultura.png", 
+    "./img/meio-ambiente-e-estilo-de-vida.png", 
+    "./img/ciencia-e-tecnologia.png", 
+    "./img/saude-e-bem-estar.png", 
+    "./img/economia-e-financas.png"
+]
 
 titulos.forEach((titulo, key) => {
     titulo.addEventListener("click", () => {
         if (corpos[key].classList.contains("hide")) {
             hideAll()
-            corpos[key].classList.toggle("hide")
+            updateBackground(key + 1)
+            corpos[key].classList.remove("hide")
         } else {
+            updateBackground()
             hideAll()
         }
     })
@@ -16,4 +27,9 @@ function hideAll(){
     corpos.forEach(e => {
         e.classList.add("hide")
     })
+}
+
+function updateBackground(key){
+    if(key == undefined) key = 0        
+    document.querySelector("body").style.backgroundImage = 'url("' + backgroundList[key] + '"'
 }
