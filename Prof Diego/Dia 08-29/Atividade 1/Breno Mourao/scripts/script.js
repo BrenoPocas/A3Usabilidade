66aacf3c0219b0fed3803541b38b8eaad1dbc028
BrenoPocas <brenomouraopocas@gmail.com>
2024-09-01 12:32:02 -0300

    conteudo html adicionado

--- Prof Diego/Dia 08-29/Atividade 1/Breno Mourao/scripts/script.js
+++ Prof Diego/Dia 08-29/Atividade 1/Breno Mourao/scripts/script.js
@@ -3,6 +3,17 @@ let corpos = document.querySelectorAll(".corpo")
 
 titulos.forEach((titulo, key) => {
     titulo.addEventListener("click", () => {
-        corpos[key].classList.toggle("hide")
+        if (corpos[key].classList.contains("hide")) {
+            hideAll()
+            corpos[key].classList.toggle("hide")
+        } else {
+            hideAll()
+        }
     })
 });
+
+function hideAll(){
+    corpos.forEach(e => {
+        e.classList.add("hide")
+    })
+}
