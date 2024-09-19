038fd94401a59d79f04139feaff6d02efd6c7d13
Lorrayne <lorrayne.santana2022@gmail.com>
2024-09-19 19:18:46 -0300

    atividade lorrayne

---
+++ Prof Diego/Dia 08-29/Atividade 2/Lorrayne Santana/javascript.js
@@ -0,0 +1,12 @@
+//window.onload = () => {
+//    const listaImagem = document.getElementsByClassName('id');
+//    for (const imagem of listaImagem) {
+//        imagem.addEventListener('mouseenter', () => {
+//            imagem.style.transform = 'scale(1.2)';
+//            imagem.addEventListener('mouseleave', () => {
+//                imagem.style.transform = 'scale(1.0)';
+//
+//            })
+//        });
+//    }
+//}
